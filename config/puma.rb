a229cc92c04fa5ab2d742967fe563a06fa388ef7
Emmanuel Paul <emmapaul247@gmail.com>
2022-12-06 08:59:44 +0100

    Prepare files for deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -32,7 +30,7 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
--- db/migrate/20221205141003_create_articles.rb
+++ db/migrate/20221205141003_create_articles.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
