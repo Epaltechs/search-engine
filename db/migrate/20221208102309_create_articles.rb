860c2cf8ee506fea521173fa5654f828eb38e345
Emmanuel Paul <emmapaul247@gmail.com>
2022-12-08 11:51:10 +0100

    Modfy article.html file

--- db/migrate/20221208102309_create_articles.rb
+++ db/migrate/20221208102309_create_articles.rb
@@ -1,7 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :name
+      t.string "title", null: false
+      t.text "body", null: false
 
       t.timestamps
     end
